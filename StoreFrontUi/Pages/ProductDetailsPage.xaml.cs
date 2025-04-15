239180cff40ee52ae70be37471849f0d2ae4dd3e
jonah3d <jonathana431@hotmail.com>
2025-04-15 13:53:48 +0200

    Product Page

--- StoreFrontUi/Pages/ProductDetailsPage.xaml.cs
+++ StoreFrontUi/Pages/ProductDetailsPage.xaml.cs
@@ -21,41 +21,116 @@ namespace StoreFrontUi.Pages
    
         public partial class ProductDetailsPage : Page
         {
-            public ObservableCollection<ProductVariant> Variant { get; set; }
+        public ObservableCollection<ProductVariant> Variant { get; set; }
+        private int _currentImageIndex = 0;
+        private SizeStock _selectedSize;
 
-            public ProductDetailsPage(Product product)
-            {
-                InitializeComponent();
-                StoreProduct = product;
-                Variant = new ObservableCollection<ProductVariant>(product.Variants);
+        public Product StoreProduct
+        {
+            get { return (Product)GetValue(StoreProductProperty); }
+            set { SetValue(StoreProductProperty, value); }
+        }
 
+        public static readonly DependencyProperty StoreProductProperty =
+            DependencyProperty.Register("StoreProduct", typeof(Product), typeof(ProductDetailsPage), new PropertyMetadata(null));
 
-                if (product.Variants != null && product.Variants.Count > 0)
+        public ProductVariant SelectedVariant
+        {
+            get { return (ProductVariant)GetValue(SelectedVariantProperty); }
+            set { SetValue(SelectedVariantProperty, value);
+                _currentImageIndex = 0;
+
+                // Update available sizes when variant changes
+                if (value != null && value.Sizes != null)
                 {
-                    SelectedVariant = product.Variants[0];
+                    AvailableSizes = new ObservableCollection<SizeStock>(value.Sizes);
+                    // Set default selected size
+                    SelectedSize = value.Sizes.FirstOrDefault(s => s.Stock > 0);
                 }
+
+            }
+        }
+
+        public static readonly DependencyProperty SelectedVariantProperty =
+            DependencyProperty.Register("SelectedVariant", typeof(ProductVariant), typeof(ProductDetailsPage), new PropertyMetadata(null));
+
+
+        public ObservableCollection<SizeStock> AvailableSizes
+        {
+            get { return (ObservableCollection<SizeStock>)GetValue(AvailableSizesProperty); }
+            set { SetValue(AvailableSizesProperty, value); }
+        }
+
+        public static readonly DependencyProperty AvailableSizesProperty =
+            DependencyProperty.Register("AvailableSizes", typeof(ObservableCollection<SizeStock>), typeof(ProductDetailsPage), new PropertyMetadata(null));
+
+
+
+        public SizeStock SelectedSize
+        {
+            get { return (SizeStock)GetValue(SelectedSizeProperty); }
+            set { SetValue(SelectedSizeProperty, value); }
+        }
+
+        public static readonly DependencyProperty SelectedSizeProperty =
+            DependencyProperty.Register("SelectedSize", typeof(SizeStock), typeof(ProductDetailsPage), new PropertyMetadata(null));
+
+        public string CurrentImageUrl
+        {
+            get
+            {
+                if (SelectedVariant?.Photos != null && SelectedVariant.Photos.Count > _currentImageIndex)
+                {
+                    return SelectedVariant.Photos[_currentImageIndex].Url;
+                }
+                return null;
+            }
+        }
+        public bool HasDiscount
+        {
+            get
+            {
+                return SelectedVariant?.Discount != null &&
+                       SelectedVariant.Discount.Percentage > 0 &&
+                       (SelectedVariant.Discount.ValidFrom == null || DateTime.Now >= SelectedVariant.Discount.ValidFrom) &&
+                       (SelectedVariant.Discount.ValidTo == null || DateTime.Now <= SelectedVariant.Discount.ValidTo);
+            }
+        }
+
+        // Discounted price property
+        public decimal DiscountedPrice
+        {
+            get
+            {
+                if (HasDiscount && SelectedVariant != null)
+                {
+                    return SelectedVariant.Price * (1 - (decimal)SelectedVariant.Discount.Percentage / 100);
+                }
+                return 0;
+            }
+        }
+
+        public ProductDetailsPage(Product product)
+        {
+            InitializeComponent();
+            StoreProduct = product;
+            Variant = new ObservableCollection<ProductVariant>(product.Variants);
+
+
+            if (product.Variants != null && product.Variants.Count > 0)
+            {
+                SelectedVariant = product.Variants[0];
+
+                if (SelectedVariant.Sizes != null && SelectedVariant.Sizes.Count > 0)
+                {
+                    SelectedSize = SelectedVariant.Sizes.FirstOrDefault(s => s.Stock > 0);
+                }
+
 
                 this.DataContext = this;
             }
 
-            public Product StoreProduct
-            {
-                get { return (Product)GetValue(StoreProductProperty); }
-                set { SetValue(StoreProductProperty, value); }
-            }
-
-            public static readonly DependencyProperty StoreProductProperty =
-                DependencyProperty.Register("StoreProduct", typeof(Product), typeof(ProductDetailsPage), new PropertyMetadata(null));
-
-            public ProductVariant SelectedVariant
-            {
-                get { return (ProductVariant)GetValue(SelectedVariantProperty); }
-                set { SetValue(SelectedVariantProperty, value); }
-            }
-
-            public static readonly DependencyProperty SelectedVariantProperty =
-                DependencyProperty.Register("SelectedVariant", typeof(ProductVariant), typeof(ProductDetailsPage), new PropertyMetadata(null));
-
+        }
             private void LB_ProductVariants_SelectionChanged(object sender, SelectionChangedEventArgs e)
             {
    
@@ -64,5 +139,45 @@ namespace StoreFrontUi.Pages
                     SelectedVariant = selectedVariant;
                 }
             }
+        private void LB_Sizes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Update selected size when size is selected
+            if (LB_Sizes.SelectedItem is SizeStock selectedSize)
+            {
+                SelectedSize = selectedSize;
+            }
         }
+
+        private void PreviousImage_Click(object sender, RoutedEventArgs e)
+        {
+            if (SelectedVariant?.Photos != null && SelectedVariant.Photos.Count > 0)
+            {
+                _currentImageIndex--;
+                if (_currentImageIndex < 0)
+                {
+                    _currentImageIndex = SelectedVariant.Photos.Count - 1;
+                }
+                UpdateImageSource();
+            }
+        }
+
+        private void NextImage_Click(object sender, RoutedEventArgs e)
+        {
+            if (SelectedVariant?.Photos != null && SelectedVariant.Photos.Count > 0)
+            {
+                _currentImageIndex++;
+                if (_currentImageIndex >= SelectedVariant.Photos.Count)
+                {
+                    _currentImageIndex = 0;
+                }
+                UpdateImageSource();
+            }
+        }
+
+        private void UpdateImageSource()
+        {
+            // Update image source using binding
+            Product_Img.GetBindingExpression(Image.SourceProperty)?.UpdateTarget();
+        }
+    }
     }
